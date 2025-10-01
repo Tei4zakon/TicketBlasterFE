728da711b6b7f5eee34fa3e39a06cb888c7311f5
Tei4zakon <dimitrova.tea@gmail.com>
2025-10-01 02:10:41 +0200

    private route configuration

--- src/utils/PrivateRoute.jsx
+++ src/utils/PrivateRoute.jsx
@@ -1,13 +1,13 @@
 
 import React from 'react';
-import {Outlet,Navigate} from 'react-router';
-import {useSelector} from 'react-redux';
+import {Outlet,Navigate} from 'react-router-dom';
 import {getToken} from './../config/StorageFunctions';
+import { isExpired } from 'react-jwt';
 
 const PrivateRoute = ({redirectPath='/login',children}) =>{
-    const token = useSelector(state=>state.authReducer.token)
+
     const localStorageToken = getToken();
-    if(!token && !localStorageToken){
+    if(!localStorageToken || isExpired(localStorageToken)){
         return <Navigate to={redirectPath}/>
     }
     return children ? children : <Outlet/>
