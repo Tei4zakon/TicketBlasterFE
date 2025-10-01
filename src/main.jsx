5cd8e7425fbfa546162c94319c88bdf4944b0a0a
Tei4zakon <dimitrova.tea@gmail.com>
2025-10-01 02:11:09 +0200

    adding new routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // or 'react-dom' for older versions
-import App from './components/App';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
-import LoginPage from './components/LoginPage';
-import CreateAccountPage from './components/CreateAccountPage';
-import ForgotPasswordPage from './components/ForgotPasswordPage';
-import ResetPasswordPage from './components/ResetPasswordPage';
-//import { Provider } from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+import store from "./store.jsx";
+import App from "./components/App";
+import LoginPage from "../src/components/auth/LoginPage.jsx";
+//import LoggedUser from "./components/LoggedUserPage.jsx";
+import CreateAccountPage from "./components/CreateAccountPage";
+import ForgotPasswordPage from "./components/ForgotPasswordPage";
+import ResetPasswordPage from "./components/ResetPasswordPage";
+import { Events } from "./components/events/index.jsx";
+import PrivateRoute from "./utils/PrivateRoute.jsx";
+import LoggedUser from "./components/LoggedUserPage.jsx";
 
 
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
     <Router>
-        <Routes>
-            <Route path='/' element={<App/>}>
-                <Route path='/login' element={<LoginPage/>}></Route>
-                <Route path='/create-account' element={<CreateAccountPage/>}></Route>
-                <Route path='login/forgot-password' element={<ForgotPasswordPage/>}></Route>
-                <Route path='login/forgot-password/reset-password' element={<ResetPasswordPage/>}></Route>
-                
-            </Route>
-            
-            
-        </Routes>
+      <Routes>
+        <Route path="/create-account" element={<CreateAccountPage />} />
+        <Route path="/login" index element={<LoginPage />} />
+        <Route path="/events" element={<Events />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <App />
+            </PrivateRoute>
+          }
+        >
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path='/reset-password' element={<ResetPasswordPage/>} />
+          <Route path="/user" element={<LoggedUser/>}>
+            <Route path="/user/ticket-history" />
+            <Route path="/user/user-details" />
+          </Route>
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </Router>
-    );
+  </Provider>
+);
